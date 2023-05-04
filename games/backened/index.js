0cc1a30e193a0c0988a7bc3b80373e1858c23ec3
Bhavye2003Developer <bhavyedevelopment2003@gmail.com>
2023-05-04 21:01:27 +0530

    All projects

--- games/backened/index.js
+++
@@ -1,7 +0,0 @@
-const canvas = document.getElementById("canvas")
-const context = canvas.getContext("2d")
-
-window.addEventListener("mousemove", (event) => {
-    console.log(event)
-    context.fillRect(event.clientX, event.clientY, 10,10)
-})
